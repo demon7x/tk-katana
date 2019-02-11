20e420a72cab195655071a31625de4e332887e3d
demon7x <demon7x@westworld.co.kr>
2019-02-11 14:41:06 +0900

    3.0 modify

--- hooks/scene_operation_tk-katana.py
+++ hooks/scene_operation_tk-katana.py
@@ -4,81 +4,71 @@
 import os
 from Katana import KatanaFile
 
-import tank
-from tank import Hook
-from tank import TankError
-from tank.platform.qt import QtGui
+import sgtk
+from sgtk.platform.qt import QtGui
 
-# TODO: Should this be given a better name
-import engine
+HookClass = sgtk.get_hook_baseclass()
 
-class SceneOperation(Hook):
+print HookClass
+
+class SceneOperation(HookClass):
     """
-    Hook called to perform an operation with the 
-    current scene.
+    Hook called to perform an operation with the
+    current scene
     """
-    
+
     def execute(self, operation, file_path, context, parent_action, file_version, read_only, **kwargs):
         """
         Main hook entry point
-        
+
         :param operation:       String
                                 Scene operation to perform
-        
+
         :param file_path:       String
                                 File path to use if the operation
                                 requires it (e.g. open)
-                    
+
         :param context:         Context
                                 The context the file operation is being
                                 performed in.
-                    
+
         :param parent_action:   This is the action that this scene operation is
                                 being executed for.  This can be one of:
                                 - open_file
                                 - new_file
-                                - save_file_as 
+                                - save_file_as
                                 - version_up
-                        
+
         :param file_version:    The version/revision of the file to be opened.  If this is 'None'
                                 then the latest version should be opened.
-        
+
         :param read_only:       Specifies if the file should be opened read-only or not
-                            
+
         :returns:               Depends on operation:
                                 'current_path' - Return the current scene
                                                  file path as a String
-                                'reset'        - True if scene was reset to an empty 
+                                'reset'        - True if scene was reset to an empty
                                                  state, otherwise False
                                 all others     - None
         """
         
-        if file_path:
-            file_path = file_path.replace("/", os.path.sep)
-        
+
         if operation == "current_path":
-            # return the current script path
-            return "/path/here".replace("/", os.path.sep) # TODO!!
-        
+            # return the current scene path
+            return file_path
         elif operation == "open":
-            # open the specified script
             KatanaFile.Load(file_path)
-            
         elif operation == "save":
-            # save the current script:
             if not os.path.exists(os.path.dirname(file_path)):
                 os.makedirs(os.path.dirname(file_path))
-            KatanaFile.Save(file_path) # TODO: warning: this may not work...
-            
+            KatanaFile.Save(file_path)
         elif operation == "save_as":
             if not os.path.exists(os.path.dirname(file_path)):
                 os.makedirs(os.path.dirname(file_path))
-            KatanaFile.Save(file_path)   
-            
+            KatanaFile.Save(file_path)
+
         elif operation == "reset":
-            """
-            Reset the scene to an empty state
-            """
+
             while KatanaFile.IsFileDirty():
                 # Changes have been made to the scene
                 res = QtGui.QMessageBox.question(None,
@@ -93,19 +83,5 @@ class SceneOperation(Hook):
                 else:
                     if not os.path.exists(os.path.dirname(file_path)):
                         os.makedirs(os.path.dirname(file_path))
-                    KatanaFile.Save(file_path) # TODO: warning: this may not work...
+                KatanaFile.Save(file_path) # TODO: warning: this may not work...
             return True
-
-        # Create a new engine instance from the existing one, with the new context
-        # Specify "shot_step" environment if the current context specifies a step
-        if context.step:
-            env = context.tank.pipeline_configuration.get_environment("shot_step", context=context) 
-        # Otherwise, just get a "project" level context
-        else:
-            env = context.tank.pipeline_configuration.get_environment("project", context=context)
-
-        name = "tk-katana" # TODO: Get this properly?
-        new_katana_engine = engine.KatanaEngine(context.tank, context, name, env)
-        new_katana_engine.add_katana_menu()
-
-
