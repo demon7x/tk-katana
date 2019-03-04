772dc0cc4a7887aea2c098c47ff1b4d9a28ca478
demon7x <demon7x@westworld.co.kr>
2019-03-04 14:50:20 +0900

    panels fix

---
+++ python/tk_katana/panels.py
@@ -0,0 +1,33 @@
+from UI4.Tabs import BaseTab
+from sgtk.platform.qt import QtCore, QtGui
+
+
+
+
+class KatanaPanelWidget(BaseTab):
+
+    def __init__(self,bundle, dialog_name, panel_id, widget_class, *args, **kwargs):
+        BaseTab.__init__(self, None)
+        hLayout = QtGui.QHBoxLayout()
+        hLayout.setObjectName('hLayout')
+        hLayout.addStretch()
+        print widget_class
+        print bundle
+        print panel_id
+        print dialog_name
+        dialog = Test(bundle, dialog_name, panel_id, widget_class)
+
+
+
+
+
+class Test(QtGui.QWidget):
+
+    def __init__(self,bundle, dialog_name, panel_id, widget_class):
+        
+        hLayout = QtGui.QHBoxLayout()
+        hLayout.setObjectName('hLayout')
+        hLayout.addStretch()
+        #widget = widget_class()
+        #hLayout.addWidget(widget)
+        
